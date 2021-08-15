f2adfe33249d79d0bfa035fc2f2aeec2e50afe10
wolfraine <lukaszsykula@gmail.com>
2021-08-15 11:05:18 +0200

    Działa Play tylko i wyłącznie

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PythonMusicPlayer
- 
+# Python Music Player is program to listen to audio files, to create simple play list 
--- player.py
+++ player.py
@@ -3,13 +3,23 @@ import time #potrzebna funkcja sleep
 from tinytag import TinyTag, TinyTagException#biblioteka do odczytywania tagów -> autor, czas trwania, itp.
 from tkinter import * #moduł do tworzenia interfejsu graficznego
 
+# obsługa grafiki
+root = Tk()
+root.title("Python Simple Playerf")
+root.geometry()
+#Creating a Label Wiget
+#myLabel = Label(root, text="hello world")
+#Showing label on screen
+#myLabel.pack()
+
+# obsługa muzyki
 file='Alex2.mp3'
 file3='fire.wav'
-player = vlc.MediaPlayer(file)
 #player puszcza muzykę ale potrzebny jest sleep, długość sleepa oznacza przez jaki czas będzie grany plik, 
 #potrzebne wyciągnięcie ile trwa plik !!!
-
-
+def musicPlayer():
+    player = vlc.MediaPlayer(file)
+    return player.play()
 
 def music_tag(song_file):
     tag = TinyTag.get(file)
@@ -23,6 +33,16 @@ def songtime(song_file): #zwraca czas trwania utworu w sekundach
     tag = TinyTag.get(file)
     return tag.duration
 
+
+myButton = Button(root, text="Play", command=musicPlayer)
+myButton1 = Button(root, text="Pause")
+myButton.grid(row=0, column=0)
+myButton1.grid(row=0, column=1)
+root.mainloop()
+
+
+
 music_tag(file)
-player.play()
-time.sleep(songtime(file))
+#player.play()
+#time.sleep(songtime(file))
+
