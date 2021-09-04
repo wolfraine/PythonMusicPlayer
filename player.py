64c3dd9eb17fc10be0ebb2cc61e304e269f09f25
wolfraine <lukaszsykula@gmail.com>
2021-09-05 00:35:33 +0200

    Working button next & prev

--- player.py
+++ player.py
@@ -52,10 +52,31 @@ def btn_stop(): #def stopu
     var.set(tagDefvalue)
 
 def btn_next():
-    pass
+    next_file = fileListFrame.curselection()
+    next_file = next_file[0] + 1
+    # grab file title from playlist
+    song = fileListFrame.get(next_file)
+    #load file and play
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+    #move active bar in playlist
+    fileListFrame.select_clear(0, END)
+    fileListFrame.activate(next_file)
+    fileListFrame.selection_set(next_file, last=None)
+
 
 def btn_prev():
-    pass
+    next_file = fileListFrame.curselection()
+    next_file = next_file[0] - 1
+    # grab file title from playlist
+    song = fileListFrame.get(next_file)
+    #load file and play
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+    #move active bar in playlist
+    fileListFrame.select_clear(0, END)
+    fileListFrame.activate(next_file)
+    fileListFrame.selection_set(next_file, last=None)
 
 #Audio files tag's
 def music_tag(song_file):
@@ -71,12 +92,14 @@ def add_song():
     song = filedialog.askopenfilename(initialdir='audio/', title="Choose song", filetypes=(("mp3 Files", "*.mp3"), ))
     fileListFrame.insert(END, song)
 
-#print tags -> add command to print info on click on file ?
+def add_songs():
+    songs = filedialog.askopenfilenames(initialdir='audio/', title="Choose song", filetypes=(("mp3 Files", "*.mp3"), ))
+    for song in songs:
+        fileListFrame.insert(END, song)
 
+#tags print label
 label_music_tag = tk.Label(master=audioTagBox, textvariable=var, relief=RAISED, justify=LEFT, width=45, anchor='w', bg='white')
-#var.set(music_tag('Alex2.mp3'))
 label_music_tag.pack()
-
 
 #create panel control button
 controlFrame = tk.Frame(root)
@@ -106,7 +129,7 @@ file_menu.add_command(label = "Exit")
 
 #add song menu
 player_menu = tk.Menu(my_menu)
-my_menu.add_cascade(label = "Add songs", menu = player_menu)
+my_menu.add_cascade(label = "Add song/songs", menu = player_menu)
 player_menu.add_command(label = "Add one Song to Playlist",command=add_song)
-
+player_menu.add_command(label = "Add many Songs to Playlist",command=add_songs)
 root.mainloop()
