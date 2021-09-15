34a81ddca52fdcf3a819512cf0d6abe15e5efcf0
wolfraine <lukaszsykula@gmail.com>
2021-09-15 17:49:27 +0200

    Added icon and backgroud Image

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
-from tkinter.constants import ACTIVE, ANCHOR, END, FIRST, LAST, LEFT, RAISED, VERTICAL, W
+from tkinter.constants import ACTIVE, ANCHOR, BOTH, END, FIRST, LAST, LEFT, RAISED, RIGHT, TOP, VERTICAL, W
 import tkinter as tk #module to create grapgic interface
-from tkinter import PhotoImage, ttk
-from tkinter import filedialog
-import time 
+from tkinter import ttk, filedialog, PhotoImage
+import time
+from typing import Any 
 from tinytag import TinyTag, TinyTagException 
 import pygame
 from PIL import ImageTk, Image
@@ -10,43 +10,41 @@ from PIL import ImageTk, Image
 # graphic 
 root = tk.Tk()
 root.title("Python Simple Player") # Title of window
-root.geometry("550x450") #basic size of window
-root.wm_attributes('-transparentcolor', '#ab23ff')
+root.geometry("500x450") #basic size of window
+
+#root.iconbitmap('icon.ico') dont work for me 
+img_icon = PhotoImage(file='icon.png')
+root.tk.call('wm', 'iconphoto', root._w, img_icon)
+
+#I want to this project static window that why i block resize
+root.resizable(False, False)
+
 global paused #global value for pausing of file 
 paused = False 
 
+bg = ImageTk.PhotoImage(Image.open('foto1.png').resize((500, 450), Image.ANTIALIAS))
 
-bg = PhotoImage(file = "foto1.png")
-
-#=====================================================
-#img = ImageTk.PhotoImage(Image.open('foto1.png').resize((550, 450), Image.ANTIALIAS))
-#lbl = tk.Label(root, image=img)
-#lbl.img = img  # Keep a reference in case this code put is in a function.
-#lbl.place(relx=0.5, rely=0.5, anchor='center')  # Place label in center of parent.
-
-
-#========================================================
 #init pygame
 pygame.mixer.init()
 
 #Create master Frame
-masterFrame = tk.Label(root)
-masterFrame.pack(pady=30, anchor='center')
+masterFrame = tk.Label(root, image=bg)
+masterFrame.pack(anchor='center', fill=BOTH, expand=True)
 
 #List of audio files
 fileListFrame = tk.Listbox(masterFrame, bg="aqua", fg="green", width=60)
-fileListFrame.grid(row=0, column=0, columnspan=2)
+fileListFrame.grid(row=0, column=0, columnspan=2, padx=60, pady=20)
 
 #button for AudioList
-fileListButton = tk.Frame(masterFrame, bg="blue")
-fileListButton.grid(row=1, column=0, columnspan=2, pady=5)
+fileListButton = tk.Frame(masterFrame)
+fileListButton.grid(row=1, column=0, columnspan=2)
 
 #box to print audioFile tag's
 audioTagBox = tk.Listbox(masterFrame, fg="green")
-audioTagBox.grid(row=2, column=0, pady=10)
+audioTagBox.grid(row=2, column=0, pady=20, padx=10)
 
 volumeFrame = tk.LabelFrame(masterFrame, text="Volume")
-volumeFrame.grid(row=2, column=1, padx=20)
+volumeFrame.grid(row=2, column=1)
 
 var = tk.StringVar()  #value of label audioTagBox
 tagDefvalue = 'Title: ' + '\n' + 'Author: ' + '\n' + 'Album: ' +'\n' + 'Genre:' +'\n' + 'Time: ' 
@@ -137,6 +135,12 @@ def add_songs():
     for song in songs:
         fileListFrame.insert(END, song)
 
+def remove_song():
+    fileListFrame.delete(ACTIVE)
+
+def clear_fileList():
+    fileListFrame.delete(0, END)
+
 def volume(x):
     pygame.mixer.music.set_volume(volumeSlider.get())
     currentVolume = pygame.mixer.music.get_volume() * 100
@@ -158,7 +162,7 @@ button_prev = tk.Button(controlFrame, text="<<", command=btn_prev)
 button_next = tk.Button(controlFrame, text=">>", command=btn_next) 
 
 button_add = tk.Button(fileListButton, text="+", command=add_songs)
-button_remove = tk.Button(fileListButton, text="-")
+button_remove = tk.Button(fileListButton, text="-", command=remove_song)
 #button position
 button_prev.grid(row=0, column=0)
 button_stop.grid(row=0, column=1)
@@ -182,16 +186,20 @@ my_menu = tk.Menu(root)
 root.config(menu=my_menu)
 
 #file menu
-file_menu = tk.Menu(my_menu)
+file_menu = tk.Menu(my_menu, tearoff=0)
 my_menu.add_cascade(label = "File", menu = file_menu)
-file_menu.add_command(label = "Exit")
+file_menu.add_command(label = "Exit", command=root.quit)
 
 #add song menu
-player_menu = tk.Menu(my_menu)
+player_menu = tk.Menu(my_menu, tearoff=0)
 my_menu.add_cascade(label = "Add song/songs", menu = player_menu)
 player_menu.add_command(label = "Add one Song to Playlist", command=add_song)
 player_menu.add_command(label = "Add many Songs to Playlist", command=add_songs)
 
-
+#remove song menu
+remove_menu = tk.Menu(my_menu, tearoff=0)
+my_menu.add_cascade(label="Remove song/songs", menu=remove_menu)
+remove_menu.add_command(label="Remove Song", command=remove_song)
+remove_menu.add_command(label="Clear list of songs", command=clear_fileList)
 
 root.mainloop()
